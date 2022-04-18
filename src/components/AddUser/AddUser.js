eb223805a9f8202bf11e92de33e71a9095fa49c5
Programming Hero <programming.hero1@gmail.com>
2022-04-17 21:11:55 -0500

    initial setup of the client side

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h2>Please add a new User</h2>
+        </div>
+    );
+};
+
+export default AddUser;
