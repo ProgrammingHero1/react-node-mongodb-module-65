dac411aab1f9d6ba291458984c3194bd90c14305
Programming Hero <programming.hero1@gmail.com>
2022-04-17 21:46:26 -0500

    post data from client to the server

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const AddUser = () => {
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = {name, email};
+
+        // send data to the server
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('users added successfully!!!');
+            event.target.reset();
+        })
+    }
     return (
         <div>
             <h2>Please add a new User</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" placeholder='Email' required />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };
